3153fed4d489b1e534980c7070c3060753120008
masamichhhhi <masamichhhi@gmail.com>
2022-03-12 16:38:10 +0900

    First commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/masamichhhhi/demo-search-engine
+
+go 1.17
+
+require github.com/masamichhhhi/go-search-engine v0.0.0-20220311183636-9c7f0c573c70
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/ikawaha/kagome-dict v1.0.4 // indirect
+	github.com/ikawaha/kagome-dict-ipa-neologd v0.2.0 // indirect
+	github.com/ikawaha/kagome-dict-ipa-neologd/internal/mod0 v0.2.0 // indirect
+	github.com/ikawaha/kagome-dict-ipa-neologd/internal/mod1 v0.2.0 // indirect
+	github.com/ikawaha/kagome/v2 v2.7.0 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/kotaroooo0/gojaconv v0.0.0-20210223133819-8a8c2bab5241 // indirect
+	golang.org/x/exp v0.0.0-20200331195152-e8c3332aa8e5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+
+	gosearchengine "github.com/masamichhhhi/go-search-engine"
+)
+
+func main() {
+	db, _ := gosearchengine.NewDBClient(
+		// &gosearchengine.DBConfig{
+		// 	"root",
+		// 	"password",
+		// 	"127.0.0.1",
+		// 	"13306",
+		// 	"gosearchengine",
+		// },
+		&gosearchengine.DBConfig{
+			User:     "root",
+			Password: "password",
+			Addr:     "127.0.0.1",
+			Port:     "13306",
+			DB:       "gosearchengine",
+		},
+	)
+	storage := gosearchengine.NewStorageRdbImpl(db)
+	analyzer := gosearchengine.NewAnalyzer(
+		[]gosearchengine.CharFilter{},
+		gosearchengine.NewStandardTokenizer(),
+		[]gosearchengine.TokenFilter{
+			gosearchengine.LowerCaseFilter{},
+		},
+	)
+
+	// 転置インデックスに登録
+	indexer := gosearchengine.NewIndexer(storage, analyzer, 1)
+	for _, body := range []string{
+		"Ruby PHP JS",
+		"Go Ruby",
+		"Ruby Go PHP",
+		"Go PHP",
+	} {
+		indexer.AddDocument(
+			gosearchengine.NewDocument(body),
+		)
+	}
+
+	// or検索を実行
+	sorter := gosearchengine.NewTfIdSorter(storage)
+	mq := gosearchengine.NewMatchQuery(
+		"GO Ruby",
+		gosearchengine.OR,
+		analyzer,
+		sorter,
+	)
+
+	msearcher := mq.Searcher(storage)
+	result, _ := msearcher.Search()
+	fmt.Println(result)
+
+	// フレーズ検索を実行
+	pq := gosearchengine.NewPhraseQuery(
+		"go RUBY", analyzer, nil)
+	psearcher := pq.Searcher(storage)
+	result, _ = psearcher.Search()
+	fmt.Println(result)
+}
